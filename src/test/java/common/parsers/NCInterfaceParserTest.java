cf3b13b038415e146944c01519cfc1ab70b38655
Exslims <exslims1@mail.ru>
2016-06-10 15:32:19 +0400

    added junit

---
+++ src/test/java/common/parsers/NCInterfaceParserTest.java
@@ -0,0 +1,15 @@
+package common.parsers;
+
+import static org.junit.Assert.*;
+
+/**
+ * user: koku0316
+ * date: 10/06/2016.
+ */
+public class NCInterfaceParserTest {
+
+    @org.junit.Test
+    public void getJSDocsOf() throws Exception {
+
+    }
+}
